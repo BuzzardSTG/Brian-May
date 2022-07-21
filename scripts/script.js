f71b92228d13b2fb7f57ac9983a1388d651c5d37
Valerijs Kanevs <valerijs.kanevs@icloud.com>
2022-07-22 01:29:35 +0900

    added Spotify playlist

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/* ===================== Jave Script for opening of a menu =============*/
+var navlinks = document.getElementById("navLinks");
+         function showMenu(){
+            navLinks.style.right = "0";
+         }
+
+         function hideMenu(){
+            navLinks.style.right = "-200px";
+         }
